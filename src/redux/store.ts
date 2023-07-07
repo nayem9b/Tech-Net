d3a4a39728fdd68b9422ac23ed9bc3c8c0760ae5
nayem9b <razwanniam1@gmail.com>
2023-07-07 17:19:52 +0600

    api slice middleware added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './features/cart/cartSlice';
 import productSlice from './features/products/productSlice';
+import { api } from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
     product: productSlice,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 export type RootState = ReturnType<typeof store.getState>;
 
