69ce03abcf167f1c05a1c26e7272537c7021ca97
nayem9b <razwanniam1@gmail.com>
2023-07-11 19:04:10 +0600

    handled user persistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { setLoading, setUser } from './redux/features/user/userSlice';
+import { useAppDispatch } from './redux/hooks';
+import { auth } from './lib/firebase';
 
 function App() {
+  const dispatch = useAppDispatch();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      dispatch(setUser(user.email!));
+      dispatch(setLoading(false));
+    } else {
+      dispatch(setLoading(false));
+    }
+  });
   return (
     <div>
       <Toaster />
